d1f311c4e41949acb8e645f6c34eaba5db381d77
TakeshiYada <blessing.1016.action@gmail.com>
2020-09-30 22:46:06 +0900

    update files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, foreign_key: :post_id, dependent: :destroy
+
+  validates :audio, presence: true
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
 end
